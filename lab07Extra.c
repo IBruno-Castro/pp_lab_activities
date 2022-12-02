2dc89d0a74b4d449bdc3ac994f53f3cb8a3b30f9
Bruno Castro <66138023+IBruno-Castro@users.noreply.github.com>
2022-12-02 10:10:17 -0300

    Add files via upload

---
+++ lab07Extra.c
@@ -0,0 +1,40 @@
+//Exercicio Bonus Lab07 02/12/2022
+
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char frase[200];
+
+    printf("Digite uma frase: ");
+    fgets(frase, 199, stdin);
+
+    printf("%s", frase);
+
+    for(int i = 0; i < 200; i++){
+
+        //Se o caracter for igual a R, troque por l
+        if(frase[i] == 'R'){
+            frase[i] = 'L';
+        }
+
+        //se tiverem dois r em sequecia, substitua por o primeiro por l e desloque o resto da string para tras
+        if(frase[i] == 'r' && frase[i + 1] == 'r'){
+            frase[i] = 'l';
+            for(int j = i + 1; frase[j] != '\0'; j++){
+                frase[j] = frase[j + 1];
+            }
+        }
+        else if(frase[i] == 'r' && frase[i + 1] != ' '){
+            if(frase[i + 1] != ',' && frase[i + 1] != '.' && frase[i + 1] != '!' && frase[i + 1] != '?' && frase[i + 1] != '\0'){
+                frase[i] = 'l';//se o r nao estiver no final de uma palavra, troque por l
+            }
+        }
+
+    }
+
+    printf("%s", frase);
+
+    return 0;
+}
